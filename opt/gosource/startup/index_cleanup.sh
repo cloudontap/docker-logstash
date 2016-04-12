c021d656603fe31fe793e687487efebdf4b71358
Michael Leditschke <mike@ammd.com.au>
2016-04-12 22:02:10 +1000

    Correct data comparison logic for index cleanup

--- opt/gosource/startup/index_cleanup.sh
+++ opt/gosource/startup/index_cleanup.sh
@@ -19,13 +19,13 @@ fi
 
 # Cycle through the current indexes
 for INDEX in $(curl -s "${ES}/_cat/indices?h=i" | grep "logs-" | sort); do
-    INDEX_DATE=$(echo $"INDEX" | cut -f2 -d"-" | tr "." "-")
-    AGE=$(( ($(date -u +%s) - $(date -ud "2016-04-10" +%s))/24/3600  ))
+    INDEX_DATE=$(echo ${INDEX} | cut -f2 -d"-" | tr "." "-")
+    AGE=$(( ($(date -u +%s) - $(date -ud ${INDEX_DATE} +%s))/24/3600  ))
     if [[ $? -eq 0 ]]; then
         echo "${INDEX} is ${AGE} days old"
         if [[ ${AGE} -gt ${INDEX_AGE} ]]; then
             echo "Deleting ${INDEX} ..."
-            echo curl -XDELETE "${ES}/${INDEX}"
+            curl -XDELETE "${ES}/${INDEX}"
         fi
     fi
 done
